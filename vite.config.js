dba2b296023a29601db489631e206c94e48cefda
Tyo <bayuokye@gmail.com>
2026-08-19 15:32:08 +0700

    github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // "./" agar folder dist/ juga bisa dibuka langsung lewat klik ganda index.html
-  base: "./",
+  base: "/Portofolio_Novarista",
   plugins: [react()],
 });
